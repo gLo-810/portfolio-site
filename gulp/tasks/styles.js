f35c3dedf094f1bd564bfd39f081c4ca9a455f3e
Gabe <glopez810@gmail.com>
2018-10-02 22:25:47 -0500

    adjusted spacing on about and project sections. currently adjusting padding for project section

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,12 +4,13 @@ autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
 cssImport = require('postcss-import'),
-colorFunction = require("postcss-color-function");
+colorFunction = require("postcss-color-function"),
+mixins = require("postcss-mixins");
 
 
 gulp.task('styles', () => {
   return gulp.src('./app/assets/styles/main.css')
-    .pipe(postcss([cssImport, cssvars, nested, colorFunction, autoprefixer]))
+    .pipe(postcss([cssImport, mixins, cssvars, nested, colorFunction, autoprefixer]))
     .on('error', function(errorInfo){
       console.log(errorInfo.toString());
       this.emit('end');
